2bf7d0badc726c75f6d5bf966d7155184acc1e24
amh6281 <amh6281@naver.com>
2026-01-31 18:38:43 +0900

    chore: prettier, eslint 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "팔로우트립 | 여행을 그대로 따라가다",
+  title: '팔로우트립 | 여행을 그대로 따라가다',
   description:
-    "누군가의 여행 경로를 그대로 따라가는 가장 쉬운 방법, 팔로우트립",
+    '누군가의 여행 경로를 그대로 따라가는 가장 쉬운 방법, 팔로우트립',
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="ko" suppressHydrationWarning>
+    <html lang='ko' suppressHydrationWarning>
       <head>
         <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
-          href="https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+          rel='stylesheet'
+          as='style'
+          crossOrigin='anonymous'
+          href='https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css'
         />
       </head>
       <body
-        className="antialiased"
+        className='antialiased'
         style={{
           fontFamily:
             "'Spoqa Han Sans Neo', -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif",
