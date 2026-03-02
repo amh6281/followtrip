9558fb554f64d0a67439dd22f0fecaff75c02311
amh6281 <amh6281@naver.com>
2026-03-02 15:33:22 +0900

    style: PlaceCourseLink hover 스타일 수정

--- src/components/region/place/molecules/PlaceCourseLink.tsx
+++ src/components/region/place/molecules/PlaceCourseLink.tsx
@@ -9,7 +9,7 @@ const PlaceCourseLink = ({ href, title }: PlaceCourseLinkProps) => {
   return (
     <Link
       href={href}
-      className='border-border bg-background hover:border-primary/40 rounded-xl border px-4 py-2 text-sm font-medium transition-colors'
+      className='text-foreground border-border bg-background hover:border-primary/40 hover:text-primary rounded-xl border px-4 py-2 text-sm font-medium transition-colors'
     >
       {title}
     </Link>
