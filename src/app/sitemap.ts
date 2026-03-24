4072674102719fb6b99f09346875920da885755d
amh6281 <amh6281@naver.com>
2026-03-24 18:25:04 +0900

    chore: sitemap 개선

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,28 @@
 import type { MetadataRoute } from 'next';
-import { REGIONS } from '@/constants/region';
+import {
+  courseList,
+  destinationList,
+  landingList,
+  placeList,
+} from '@/utils/selectors';
 import { absoluteUrl } from '@/utils/seo';
 
 // 검색엔진이 색인할 공개 페이지를 정의한 sitemap
 const sitemap = (): MetadataRoute.Sitemap => {
   const routes = [
     '/',
-    '/tools/budget',
-    ...Object.values(REGIONS).flatMap((region) => [
-      `/${region.id}`,
-      ...region.highlightCourseSlugs.map(
-        (courseId) => `/${region.id}/${courseId}`,
-      ),
-      ...region.placeSlugs.map((placeId) => `/${region.id}/place/${placeId}`),
-    ]),
+    ...destinationList.map(
+      (destination) => `/destinations/${destination.slug}`,
+    ),
+    ...placeList.map(
+      (place) => `/destinations/${place.regionId}/places/${place.slug}`,
+    ),
+    ...courseList.map(
+      (course) => `/destinations/${course.regionId}/courses/${course.slug}`,
+    ),
+    ...landingList
+      .filter((landing) => landing.indexable)
+      .map((landing) => `/trips/${landing.regionId}/${landing.slug}`),
   ];
 
   return routes
